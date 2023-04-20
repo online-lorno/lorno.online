13d60d483ec75be0a08e8dddc49b689b79918d60
Leonard Laput <online.lorno@gmail.com>
2023-04-20 22:41:22 +0800

    ✨ Add home page bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image'
+
 export default function PageHome() {
   return (
     <div className="flex min-h-[calc(100vh_-_113px)] w-full items-center justify-center">
+      <Image
+        src="/assets/home-splash-bg.svg"
+        alt="home-splash-bg.svg"
+        width={779}
+        height={742}
+        className="fixed left-0"
+      />
       <div className="flex flex-col">
-        <p className="text-size-label text-white">Hi all. I am</p>
+        <p className="text-size-label text-white">Hello world. I am</p>
         <p className="text-size-headline text-white">Leonard Laput</p>
         <p className="mb-20 text-size-subheadline text-color-secondary-200">
           &gt; Full Stack Developer
